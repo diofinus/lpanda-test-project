9c27985089f9ac06b422366ed4a6980b4f914474
diofinus <diofinus@gmail.com>
2016-12-30 17:18:05 +0700

    Update logic

--- src/game/main.js
+++ src/game/main.js
@@ -7,6 +7,7 @@ const SystemGfx = require('engine/gfx');
 const SystemAnime = require('engine/anime');
 const SystemInput = require('engine/input');
 const SystemPhysic = require('engine/physics');
+const AABBSolver = require('engine/physics/AABBSolver');
 
 // Requite anything else you want to use
 const BitmapText = require('engine/gfx/BitmapText');
@@ -16,12 +17,78 @@ const { filmstrip } = require('engine/gfx/utils');
 // Loading screen
 const Loading = require('game/Loading');
 
-const collision = require('game/collision');
+const Entity = require('engine/Entity');
+const { getGroupMask } = require('engine/physics');
+const Collider = require('engine/physics/Collider');
+const Graphics = require('engine/gfx/Graphics');
+
+const PlayerGroup = getGroupMask(0);
+const SolidGroup = getGroupMask(1);
 
 // Load some resources
 loader
   .add('KenPixel.fnt')
   .add('bat', 'bat.png');
+
+class MyPlayer extends Entity {
+  constructor(x, y, s) {
+    super(x, y, null);
+
+    this.gfx = AnimatedSprite({
+      textures: filmstrip(loader.resources['bat'].texture, 51, 57),
+      anims: [
+        ['fly', [0,1,2,3,4], { speed: 10 }],
+        ['atk', [5,6,7], { speed: 8, loop: false }],
+        ['hurt', [8,9,8,9,8,9], { speed: 8, loop: false }],
+        ['kill', [10,11,12,13], { speed: 8, loop: false }],
+      ],
+    });
+
+    console.log(SolidGroup);
+
+    this.coll = Collider({
+      shape: 'Box',
+      width: this.gfx.width,
+      height: this.gfx.width,
+      collisionGroup: PlayerGroup,
+      collideAgainst: SolidGroup,
+      collide: (other) => {
+        if (other.collisionGroup === SolidGroup) {
+          // Always push player back when collide against a "solid" thing
+          return true;
+        }
+      }
+    });
+  }
+}
+
+class MyBox extends Entity {
+  constructor(x, y, s = {}) {
+    super(x, y, null);
+
+    this.gfx = Graphics({
+      shape: 'Box',
+      width: s.width || 100,
+      height: s.height || 50,
+      color: s.color || 0xff2f62,
+    });
+
+    this.coll = Collider({
+      shape: 'Box',
+      width: this.gfx.width,
+      height: this.gfx.height,
+      isStatic: true,
+      collisionGroup: SolidGroup,
+      collideAgainst: PlayerGroup,
+      collide: (other) => {
+        if (other.collisionGroup === PlayerGroup) {
+          // Always push player back when collide against a "solid" thing
+          return true;
+        }
+      }
+    });
+  }
+}
 
 // A game acts like a scene/screen or whatever you call
 class MyGame extends Game {
@@ -36,38 +103,24 @@ class MyGame extends Game {
       .addSystem(new SystemAnime())
       .addSystem(new SystemGfx())
       .addSystem(new SystemInput())
-      .addSystem(new SystemPhysic());
+      .addSystem(new SystemPhysic({
+        // Physics always requires a collision solver
+        solver: new AABBSolver()
+      }));
 
     // Create some layers
     this.sysGfx
       .createLayer('background')
       .createLayer('entities')
         .createLayer('actors', 'entities')
-        .createLayer('fx', 'entities')
-        .createLayer('hud', 'entities')
       .createLayer('ui');
 
     // Add some gfx elements
-    this.text = new collision.MyText();
-    this.text.gfx.addTo(this.sysGfx.layers['background']);
-    this.text.position.set(core.width / 2 - this.text.gfx.width / 2, core.height / 2 - this.text.gfx.height / 2);
-    this.text.gfx.position.set(this.text.position.x, this.text.position.y);
+    this.text = this.spawnEntity(MyBox, core.width / 2, core.height / 2, 'actors');
 
-    this.monster = new collision.MyPlayer();
-    this.monster.gfx.addTo(this.sysGfx.layers['background']);
-    this.monster.position.set(100, 100);
+    this.monster = this.spawnEntity(MyPlayer, 100, 100, 'actors');
     this.monster.gfx.anchor.set(0.5);
     this.monster.gfx.play('fly');
-
-    console.log(this.monster);
-
-    // Animate something
-    // this.sysAnime.tween(this.monster)
-    //   .to({ 'position.x': 250 }, 2000)
-    //   .to({ 'scale.x': -1 }, 10)
-    //   .to({ 'position.x': 50 }, 2000)
-    //   .to({ 'scale.x': +1 }, 10)
-    //   .repeat(100);
 
     this.sysInput.bind('SPACE', 'attackAnim');
     this.sysInput.bind('LEFT', 'move_left');
